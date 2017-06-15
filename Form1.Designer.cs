9975175d9c1c12dabf37f2c9c3f45205a147d581
Nabid <msa.nabid007@gmail.com>
2017-06-16 02:24:43 +0600

    Boundary logic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             System.Windows.Forms.Timer vert;
             this.ball = new System.Windows.Forms.PictureBox();
-            this.slide = new System.Windows.Forms.PictureBox();
+            this.slider = new System.Windows.Forms.PictureBox();
             this.horz = new System.Windows.Forms.Timer(this.components);
             vert = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.slide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
             this.SuspendLayout();
             // 
             // ball
@@ -49,14 +49,14 @@
             this.ball.TabIndex = 0;
             this.ball.TabStop = false;
             // 
-            // slide
+            // slider
             // 
-            this.slide.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.slide.Location = new System.Drawing.Point(360, 550);
-            this.slide.Name = "slide";
-            this.slide.Size = new System.Drawing.Size(181, 16);
-            this.slide.TabIndex = 1;
-            this.slide.TabStop = false;
+            this.slider.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.slider.Location = new System.Drawing.Point(360, 550);
+            this.slider.Name = "slider";
+            this.slider.Size = new System.Drawing.Size(181, 16);
+            this.slider.TabIndex = 1;
+            this.slider.TabStop = false;
             // 
             // vert
             // 
@@ -68,12 +68,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(886, 567);
-            this.Controls.Add(this.slide);
+            this.Controls.Add(this.slider);
             this.Controls.Add(this.ball);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.slide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,7 +81,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox ball;
-        private System.Windows.Forms.PictureBox slide;
+        private System.Windows.Forms.PictureBox slider;
         private System.Windows.Forms.Timer horz;
     }
 }
