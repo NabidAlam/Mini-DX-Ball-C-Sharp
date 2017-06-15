e3386ace5a676dffd22263704c8cdfaefd3dee36
Nabid <msa.nabid007@gmail.com>
2017-06-16 03:17:41 +0600

    Left right

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,7 +18,10 @@ namespace Mini_DX_Ball_Game
             InitializeComponent();
         }
 
+        int speed = 3;
         int vdir = +1;
+        int slide_speed = 10;
+
 
         private void vert_Tick(object sender, EventArgs e)
         {
@@ -30,32 +34,94 @@ namespace Mini_DX_Ball_Game
             {
                 vdir = -1;
             }
-            else
-            {
-                ball.Top += vdir;
+           
+            
+                ball.Top += (vdir*speed);
    
-            }
+            
             
         }
 
 
-        private int hdir = +1;
+         int hdir = +1;
 
         private void horz_Tick(object sender, EventArgs e)
         {
             if (ball.Left < 0)
             {
                 hdir = +1;
+                
             }
 
-            else if (ball.Left > (this.Width-ball.Width) )
+            else if (ball.Left > (this.Width - ball.Width) )
             {
                 hdir = -1;
             }
 
-            else
+            ball.Left += (hdir*speed);
+            
+        }
+
+         bool _left = false;
+         bool _right = false;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //active
+
+            if (e.KeyCode==Keys.Right)
             {
-                ball.Height += hdir;
+                _right = true;
+            }
+
+            else if (e.KeyCode== Keys.Left)
+            {
+                _left = true;
+            }
+
+            else if (e.KeyCode== Keys.Escape)
+            {
+                //pause game
+                
+                vert.Enabled = horz.Enabled = false;
+                
+            }
+
+            else if (e.KeyCode == Keys.End)
+            {
+                //resume game
+                vert.Enabled = horz.Enabled = true;
+                
+
+            }
+
+
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //inactive
+            if (e.KeyCode==Keys.Left || e.KeyCode== Keys.Right)
+            {
+                _right = _left = false; 
+
+            }
+
+        }
+
+        private void key_watch_Tick(object sender, EventArgs e)
+        {
+            if (_right && slider.Left< (this.Width- slider.Width))
+            {
+                slider.Left = slider.Left + 10;
+
+            }
+
+            else if (_left && slider.Left > 0)
+            {
+                slider.Left = slider.Left - 10;
+
             }
         }
 
